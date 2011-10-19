f2a55bc166f812bdca060dac33eee0518aa1323c
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-19 14:13:15 +0200

    Plot data

--- plotData.m
+++ plotData.m
@@ -17,7 +17,9 @@ function plotData(x, y)
 figure; % open a new figure window
 
 
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
 
 
 
