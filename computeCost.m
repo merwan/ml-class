e1b17568651c33184cb1b01078f056b0aeb7fec2
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-19 14:13:38 +0200

    Compute cost function

--- computeCost.m
+++ computeCost.m
@@ -13,7 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predictions = X * theta;
+sqrErrors = (predictions - y).^2;
 
+J = 1 / (2*m) * sum(sqrErrors);
 
 
 
