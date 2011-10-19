9dfcbbd10e956c95817343298cf68775120d30ac
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-19 16:56:18 +0200

    First working version of gradient descent

--- gradientDescent.m
+++ gradientDescent.m
@@ -18,7 +18,10 @@ for iter = 1:num_iters
     %
 
 
-
+    htheta = X * theta;
+    theta0 = theta(1) - alpha / m * sum((htheta - y) .* X(:,1));
+    theta1 = theta(2) - alpha / m * sum((htheta - y) .* X(:,2)); 
+    theta = [theta0; theta1];
 
 
 
