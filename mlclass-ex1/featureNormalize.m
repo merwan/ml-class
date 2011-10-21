d2b2f92c4a7426595b4c957403a1e3a21b7528a3
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-21 11:06:56 +0200

    Implement feature normalize

--- mlclass-ex1/featureNormalize.m
+++ mlclass-ex1/featureNormalize.m
@@ -30,6 +30,9 @@ sigma = zeros(1, size(X, 2));
 mu = mean(X);
 sigma = std(X);
 
+for i = 1:size(X,2)
+    X_norm(:,i) = (X(:,i) - mu(i)) / sigma(i);
+end
 
 % ============================================================
 
