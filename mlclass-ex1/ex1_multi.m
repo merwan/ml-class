e8518ce5e3a7e3789b3820cd85bfad933eabb6ec
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-23 00:37:11 +0200

    Estimate price with normal equation

--- mlclass-ex1/ex1_multi.m
+++ mlclass-ex1/ex1_multi.m
@@ -150,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta;
 
 
 % ============================================================
