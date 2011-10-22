0cd70f4f735c78337fdbcb93e31a3e204507fef9
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-23 00:18:22 +0200

    Gradient descent with multiple variables

--- mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/gradientDescentMulti.m
@@ -18,10 +18,12 @@ for iter = 1:num_iters
     %
 
 
-
-
-
-
+    htheta = X * theta;
+    theta_tmp = zeros(3, 1);
+    for row = 1:size(theta, 1)
+        theta_tmp(row) = theta(row) - alpha / m * sum((htheta - y) .* X(:,row));
+    end
+    theta = theta_tmp;
 
 
 
