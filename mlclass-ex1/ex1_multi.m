f529a84b422ec90ae0ebc5b656af89c5bfca224e
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-23 00:33:28 +0200

    Estimate price with Gradient Descent

--- mlclass-ex1/ex1_multi.m
+++ mlclass-ex1/ex1_multi.m
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+price = ([1, 1650, 3] - [0 mu]) ./ [1 sigma] * theta;
 
 
 % ============================================================
