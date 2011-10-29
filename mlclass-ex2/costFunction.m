c5febe972107559d793fe07b9c57200271ba886b
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-30 00:59:27 +0200

    Calculate gradient for logistic regression

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -22,6 +22,9 @@ grad = zeros(size(theta));
 
 htheta = sigmoid(X * theta);
 J = 1 / m * sum(-y .* log(htheta) - (1 - y) .* log(1 - htheta));
+for i = 1:size(theta, 1)
+    grad(i) = 1 / m * sum((htheta - y) .* X(:, i));
+end
 
 
 
