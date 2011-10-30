22d4c605698064642d100bcc8db50761eb5b6e81
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-10-31 00:05:29 +0100

    Fix gradient

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -23,7 +23,7 @@ J = 1 / m * sum(-y .* log(htheta) - (1 - y) .* log(1 - htheta)) + lambda / (2 * 
 
 grad(1) = 1 / m * sum((htheta - y) .* X(:, 1));
 for i = 2:size(theta, 1)
-    grad(i) = 1 / m * sum((htheta - y) .* X(:, i) + lambda * theta(i));
+    grad(i) = 1 / m * sum((htheta - y) .* X(:, i)) - lambda / m * theta(i);
 end
 
 % =============================================================
