f3a0574d421a2ae6fad4389f74b875948619525b
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-11-04 07:15:22 +0100

    Train algorithm

--- mlclass-ex3/oneVsAll.m
+++ mlclass-ex3/oneVsAll.m
@@ -49,9 +49,11 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
-
-
-
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+for c = 1:num_labels
+    initial_theta = all_theta(c, :)';
+    [theta(c,:)] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+end
 
 
 
