5f669c44d7aff036a9cc5e8d6950e8a9eba3f172
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-11-04 14:47:16 +0100

    Fix cost function calculation to regularize it

--- mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3/lrCostFunction.m
@@ -39,8 +39,10 @@ grad = zeros(size(theta));
 
 
 htheta = sigmoid(X * theta);
-J = 1 / m * sum(-y .* log(htheta) - (1 - y) .* log(1 - htheta));
-grad = 1 / m * X' * (htheta - y) + lambda / (2 * m) * sum(theta(2:end).^2);
+J = 1 / m * sum(-y .* log(htheta) - (1 - y) .* log(1 - htheta)) + lambda / (2 * m) * sum(theta(2:end) .^ 2);
+temp = theta;
+temp(1) = 0;
+grad = 1 / m * (X' * (htheta - y) + lambda * temp);
 
 
 
