1968e2d93dd3f01eb2b9fcad0abc211f3c76605d
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-11-06 22:45:02 +0100

    Implement neural network feedforward

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -21,11 +21,12 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1) X];
+a2 = sigmoid(X * Theta1');
+a2 = [ones(m, 1) a2];
+htheta = sigmoid(a2 * Theta2');
 
-
-
-
-
+[temp, p] = max(htheta, [], 2);
 
 
 
