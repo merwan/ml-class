5da4c2769743dedc6ee9771f8dbda0848fcbf324
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-11-06 15:23:51 +0100

    One vs all classifier training completed

--- mlclass-ex3/oneVsAll.m
+++ mlclass-ex3/oneVsAll.m
@@ -52,7 +52,7 @@ X = [ones(m, 1) X];
 options = optimset('GradObj', 'on', 'MaxIter', 50);
 for c = 1:num_labels
     initial_theta = all_theta(c, :)';
-    [theta(c,:)] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+    [all_theta(c,:)] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
 end
 
 
