d2321f1350d169e75ce6d3c064c2b7881c8559c1
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-11-17 22:20:37 +0100

    Regularized linear regression gradient

--- mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5/linearRegCostFunction.m
@@ -23,7 +23,8 @@ htheta = X * theta;
 n = size(theta);
 J = 1 / (2 * m) * sum((htheta - y) .^ 2) + lambda / (2 * m) * sum(theta(2:n) .^ 2);
 
-
+grad = 1 / m * X' * (htheta - y);
+grad(2:n) = grad(2:n) + lambda / m * theta(2:n);
 
 
 
