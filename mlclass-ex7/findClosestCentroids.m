389e25e830c15f92960b9c63ec5c1a86f853ec39
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-12-04 15:01:08 +0100

    Find closest centroids

--- mlclass-ex7/findClosestCentroids.m
+++ mlclass-ex7/findClosestCentroids.m
@@ -22,7 +22,16 @@ idx = zeros(size(X,1), 1);
 %
 
 
-
+for i=1:length(X)
+    distance = inf;
+    for j=1:K
+        kDist = norm(X(i, :) - centroids(j, :));
+        if (kDist < distance)
+            distance = kDist;
+            idx(i) = j;
+        end
+    end
+end
 
 
 
