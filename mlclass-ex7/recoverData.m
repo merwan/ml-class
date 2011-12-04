dd4c39369bd56c934909483d8d5eceb7a7145c14
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-12-04 22:47:14 +0100

    Recover data

--- mlclass-ex7/recoverData.m
+++ mlclass-ex7/recoverData.m
@@ -21,6 +21,12 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
+for i=1:size(Z, 1)
+    v = Z(i, :)';
+    for j=1: size(U, 1)
+        X_rec(i, j) = v' * U(j, 1:K)';
+    end
+end
 
 
 % =============================================================
