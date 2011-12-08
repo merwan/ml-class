3ea71d2ac1ccc6bd1acce0de81cc29cc67c2679e
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-12-08 22:05:32 +0100

    Estimate Gaussian

--- mlclass-ex8/estimateGaussian.m
+++ mlclass-ex8/estimateGaussian.m
@@ -21,14 +21,8 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
-
-
-
-
+mu = mean(X);
+sigma2 = var(X, 1);
 
 % =============================================================
 
