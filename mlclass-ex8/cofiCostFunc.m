2d4ad882adfd97a6efff58ab8655190ca0641472
Merouane Atig <yahoocbien-dev@yahoo.fr>
2011-12-12 09:05:10 +0100

    Gradient with regularization

--- mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8/cofiCostFunc.m
@@ -46,14 +46,14 @@ for i=1:num_movies
     idx = find(R(i, :) == 1);
     Theta_temp = Theta(idx, :);
     Y_temp = Y(i, idx);
-    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp;
+    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp + lambda * X(i, :);
 end
 
 for i=1:num_users
     idx = find(R(:, i) == 1);
     X_temp = X(idx, :);
     Y_temp = Y(idx, i);
-    Theta_grad(i, :) = (X_temp * Theta(i, :)' - Y_temp)' * X_temp;
+    Theta_grad(i, :) = (X_temp * Theta(i, :)' - Y_temp)' * X_temp + lambda * Theta(i, :);
 end
 
 % =============================================================
